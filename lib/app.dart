ebef636c9e5ccc6a085253593157ab7d27adb47f
eomapps <apps@year2448.com>
2026-06-21 20:09:52 -0400

    feat: wire up auth-state router

--- lib/app.dart
+++ lib/app.dart
@@ -1,38 +1,61 @@
+import 'package:artexplorer/blocs/artwork/artwork_bloc.dart';
+import 'package:artexplorer/blocs/auth/auth_bloc.dart';
+import 'package:artexplorer/blocs/auth/auth_event.dart';
+import 'package:artexplorer/blocs/auth/auth_state.dart';
+import 'package:artexplorer/blocs/collection/collection_bloc.dart';
+import 'package:artexplorer/repositories/art_repository.dart';
+import 'package:artexplorer/repositories/auth_repository.dart';
+import 'package:artexplorer/repositories/collection_repository.dart';
+import 'package:artexplorer/screens/auth_screen.dart';
+import 'package:artexplorer/screens/browse_screen.dart';
 import 'package:artexplorer/utils/app_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppStrings.appTitle,
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: AppStrings.appTitle),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ArtworkBloc>(
+          create: (BuildContext context) => ArtworkBloc(ArtRepository()),
+        ),
+        BlocProvider<AuthBloc>(
+          create: (BuildContext context) =>
+              AuthBloc(AuthRepository())..add(CheckAuth()),
+        ),
+        BlocProvider<CollectionBloc>(
+          create: (BuildContext context) =>
+              CollectionBloc(context.read<AuthBloc>(), CollectionRepository()),
+        ),
+      ],
+      child: MaterialApp(
+        title: AppStrings.appTitle,
+        theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+        home: const AppBase(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class AppBase extends StatelessWidget {
+  const AppBase({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: .center, children: []),
-      ),
+    return BlocBuilder<AuthBloc, AuthState>(
+      builder: (context, state) {
+        return switch (state) {
+          AuthAuthenticated() => BrowseScreen(),
+          AuthLoading() => Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          ),
+          AuthUnauthenticated() => AuthScreen(),
+          AuthError() => AuthScreen(),
+        };
+      },
     );
   }
 }
