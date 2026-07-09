23c1c413d564b2bf92786cb4bde926dbde998124
eomapps <apps@year2448.com>
2026-07-08 22:42:00 -0400

    chore: hide debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: AppStrings.appTitle,
         theme: AppTheme.theme,
         home: const AppBase(),
