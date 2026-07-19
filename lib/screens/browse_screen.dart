6f44ccb2f636bdea951fafe9b0aa876041c4f318
eomapps <apps@year2448.com>
2026-07-19 15:44:56 -0400

    feat: add SignOut() to BrowseScreen

--- lib/screens/browse_screen.dart
+++ lib/screens/browse_screen.dart
@@ -1,4 +1,7 @@
+import 'package:artexplorer/blocs/auth/auth_bloc.dart';
+import 'package:artexplorer/blocs/auth/auth_event.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BrowseScreen extends StatefulWidget {
   const BrowseScreen({super.key});
@@ -10,6 +13,18 @@ class BrowseScreen extends StatefulWidget {
 class _BrowseScreenState extends State<BrowseScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Browse Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Browse Screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<AuthBloc>().add(SignOut());
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+    );
   }
 }
